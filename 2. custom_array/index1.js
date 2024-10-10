d6a26d3deae809d657a9fe7358e097b418c1c06d
lokesh shriwas <lokeshshriwas2802@gmail.com>
2024-10-11 00:07:16 +0530

    remove and shift function

--- 2. custom_array/index1.js
+++ 2. custom_array/index1.js
@@ -16,6 +16,34 @@ class MyArray{
         this.length-- 
         console.log(this.length)
     }
+
+    shift() {
+        const firstElem = this.data[0]
+        for (let i = 0; i < this.length; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+
+        delete this.data[this.length - 1]
+        this.length--
+        return firstElem
+    }
+
+    remove(idx) {
+        const item = this.data[idx];
+
+        // Shift elements left starting from idx to reindex
+        for (let i = idx; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+
+        // Delete the last element since it's now duplicated
+        delete this.data[this.length - 1];
+
+        // Reduce the length by 1
+        this.length--;
+
+        return item;
+    }
 }
 
 const newArray = new MyArray()
@@ -25,4 +53,7 @@ newArray.NewPush("banana")
 newArray.NewPush("pineapple")
 newArray.NewPush("peach")
 
-newArray.NewPop()
+// newArray.NewPop()
+console.log(newArray)
+console.log(newArray.remove(1))
+console.log(newArray)
